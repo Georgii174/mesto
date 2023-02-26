f1293b6a18caa09f279ec691f588e9ea22538ce1
Georgii Anisimov <georgii_anisimov@mail.ru>
2023-02-26 21:17:27 +0500

    fix js, css html

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,6 @@ function saveButtonFormSubmit(evt) {
   jobProfail.textContent = jobInputElement.value;
   closePopup(popupElement);
 };
-
 //Карточки из масива
 function renderCards(initialCards) {
   const cardMas = initialCards.map(function (cardElement) {
@@ -26,31 +25,30 @@ function addCards() {
 
 function createCards(name, link) {
   const cardElement = cardTemp.cloneNode(true);
-  cardElement.querySelector('.group__images').src = link;
-  cardElement.querySelector('.group__name').textContent = name;
-  cardElement.querySelector('.group__images').alt = name;
+  const imgCard = cardElement.querySelector('.group__images');
+  const nameCard = cardElement.querySelector('.group__name');
+  const btDelCard = cardElement.querySelector('.group__button-del');
+  const btLikeCard = cardElement.querySelector('.group__button-like');
+  imgCard.src = link;
+  nameCard.textContent = name;
+  imgCard.alt = name;
 
   // кнопка лайк
-  cardContent.addEventListener('click', elem => {
-    if (elem.target.classList.contains('group__button-like')) {
-      elem.target.classList.toggle('group__button-like_active');
-    }
+  btLikeCard.addEventListener('click', elem => {
+    elem.target.classList.toggle('group__button-like_active');
   });
   // удаление карточки
-  cardContent.addEventListener('click', elem => {
-    if (elem.target.classList.contains('group__button-del')) {
-      const elemTarget = elem.target.closest('.group__element');
-      elemTarget.remove();
-    }
+  btDelCard.addEventListener('click', elem => {
+    const elemTarget = elem.target.closest('.group__element');
+    elemTarget.remove();
   });
+
   // открытие карточки(попап)
-  cardContent.addEventListener('click', elem => {
-    if (elem.target.classList.contains('group__images')) {
-      imgPopup.src = elem.target.closest('.group__images').src;
-      titlePopup.textContent = elem.target.closest('.group__images').alt;
-      titlePopup.alt = elem.target.closest('.group__images').alt;
-      popupOpen(popupElementImagesCard);
-    };
+  imgCard.addEventListener('click', elem => {
+    imgPopup.src = link;
+    titlePopup.textContent = name;
+    titlePopup.alt = name;
+    popupOpen(popupElementImagesCard);
   });
 
   return cardElement;
@@ -60,7 +58,7 @@ function addBtFormSubmit(evt) {
   evt.preventDefault();
   addCards();
   closePopup(popupElementCard);
-}
+};
 
 // открыть.закрыть попап
 function popupOpen(popup) {
